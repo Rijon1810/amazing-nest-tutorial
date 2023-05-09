88f2e66cd55c690422ce2f492d7f821f4f965321
Rijon <44047172+Rijon1810@users.noreply.github.com>
2023-05-09 12:58:36 +0600

    Understanding service concept

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
